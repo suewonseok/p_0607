d94b38d8ab77222dd2eeb3d60a7995f039ca576d
seowonseok <dydrlaks@gmail.com>
2018-06-07 19:21:54 +0900

    first trial

---
+++ django-cms/django-cms/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/3_2grade/webServiceProgramming/p_0607/django-cms/django-cms/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
